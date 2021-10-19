4e1f6f9752c546de71ed39854fb69ddece04cf78
1204143046 <1204143046@qq.com>
2021-10-19 13:57:33 +0800

    mergeSort

---
+++ src/com/ls/basic/sort/MergeSort.java
@@ -0,0 +1,7 @@
+package com.ls.basic.sort;
+
+
+public class MergeSort {
+
+	
+}
--- src/com/ls/test/LishengMain.java
+++ src/com/ls/test/LishengMain.java
@@ -2,6 +2,25 @@ package com.ls.test;
 
 public class LishengMain {
 	public static void main(String[] args) {
-		System.out.println("ÀîÎÄÌÒ");
+		int[][] relation= {{2,1},{0,2},{0,4},{3,4},{2,3},{1,4},
+				{2,0}};
+		System.out.println(fromKTo(4, relation,3));
+	}
+	
+	
+	
+	public static int fromKTo(int n, int[][]relation, int k){
+		int[][] dp = new int[k + 1][n + 1];
+		
+		dp[0][0] = 1;
+		for(int i = 0; i < k ; i++){
+			for(int[] edge: relation){
+				int src = edge[0];
+				int dest = edge[1];
+				dp[i + 1][dest] += dp[i][src];
+			}
+		}
+		
+		return dp[k][n];
 	}
 }
